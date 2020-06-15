58b4152abda98040bf6b586b26e5c902ce7c8191
MReeds <mattlreeds@gmail.com>
2020-06-15 15:39:29 -0500

    added home page, model files

---
+++ BirdieApp/views/connection.py
@@ -0,0 +1,2 @@
+class Connection:
+    db_path: = "/home/matt_reeds/Birdie-Capstone/Birdie/db.sqlite3"
--- BirdieApp/views/index.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, redirect
-
-def index(request):
-    """ The home page for Birdie """
-    return render(request, 'birdie_templates/index.html')

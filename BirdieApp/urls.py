013cb3071bc167082159f77b107e6841b8383c77
MReeds <mattlreeds@gmail.com>
2020-06-18 10:02:46 -0500

    can create a new bag

--- BirdieApp/urls.py
+++ BirdieApp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('parks/', park_list, name="parks"),
     path('park/form', park_form, name="park_form"),
     path('bags/', bag_list, name="bags"),
+    path('bag/form', bag_form, name='bag_form'),
     path('bags/<int:bag_id>/', bag_details, name="bag"),
     path('discs/', disc_list, name="discs"),
 ]

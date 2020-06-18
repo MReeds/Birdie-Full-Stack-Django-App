ae8c010a661426c53e6a69079ad96934a719916f
MReeds <mattlreeds@gmail.com>
2020-06-18 11:59:45 -0500

    creating discs

--- BirdieApp/urls.py
+++ BirdieApp/urls.py
@@ -6,9 +6,10 @@ app_name = 'BirdieApp'
 urlpatterns = [
     path('', home, name='home'),
     path('parks/', park_list, name="parks"),
-    path('park/form', park_form, name="park_form"),
+    path('park/form/', park_form, name="park_form"),
     path('bags/', bag_list, name="bags"),
-    path('bag/form', bag_form, name='bag_form'),
+    path('bag/form/', bag_form, name='bag_form'),
     path('bags/<int:bag_id>/', bag_details, name="bag"),
     path('discs/', disc_list, name="discs"),
+    path('disc/form/', disc_form, name='disc_form'),
 ]
